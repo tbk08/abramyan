2e55dca755f7d7c3cc5021ec9dd69d1b26cfc043
tbk <turemuratova08@gmail.com>
2022-08-05 22:57:36 +0500

    ff

--- Integer/src/Integer1.java
+++ Integer/src/Integer1.java
@@ -5,6 +5,6 @@ public class Integer1 {
         Scanner in = new Scanner(System.in);
         System.out.print("L=");
         int l=in.nextInt();
-        System.out.println("Meters="+(int)(l/100));
+        System.out.println("Meters="+(l/100));
     }
 }
